3a6a82dcb8ee9ca0e8c51364eabe88ccb135256d
Harshita shree <harshita360@gmail.com>
2020-07-30 17:32:44 +0530

    Updates

---
+++ precache-manifest.40e6fcd4e73e5a39a4669a82804cc096.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935eff02855a882d7c4ba14688bd6c4d",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "f7b26570d80d05917aa8",
+    "url": "/ecommerce/static/css/2.87d91982.chunk.css"
+  },
+  {
+    "revision": "7e7aeaf1a9f824eaac6e",
+    "url": "/ecommerce/static/css/main.19dd20e1.chunk.css"
+  },
+  {
+    "revision": "f7b26570d80d05917aa8",
+    "url": "/ecommerce/static/js/2.51c99bb1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ecommerce/static/js/2.51c99bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7aeaf1a9f824eaac6e",
+    "url": "/ecommerce/static/js/main.ae7caa9a.chunk.js"
+  },
+  {
+    "revision": "50ad373dcf2441cba62d",
+    "url": "/ecommerce/static/js/runtime-main.b8585686.js"
+  },
+  {
+    "revision": "ec13b457a74f1d615db496c8e8a6c29c",
+    "url": "/ecommerce/static/media/bg_main.ec13b457.png"
+  },
+  {
+    "revision": "f87cdbd7ad1526417a25ba45306e4fd5",
+    "url": "/ecommerce/static/media/footer_bg.f87cdbd7.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ecommerce/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ecommerce/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ecommerce/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ecommerce/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "312d0c8103982ca5d8bb1ce3327715d7",
+    "url": "/ecommerce/static/media/works-icon-01.312d0c81.png"
+  },
+  {
+    "revision": "867260194c6dd535f91675b012df44e6",
+    "url": "/ecommerce/static/media/works-icon-02.86726019.png"
+  }
+]);
